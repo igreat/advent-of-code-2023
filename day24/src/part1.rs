d6abccc23a09fe89de37b5494d529e742acc8cd2
igreat <goraguy5@gmail.com>
2023-12-24 10:13:55 +0000

    improve day24 p1 perf

--- day24/src/part1.rs
+++ day24/src/part1.rs
@@ -1,76 +1,82 @@
+use rayon::prelude::*;
+
 pub fn run(input: &str) -> u32 {
     let particles = parse_input(input);
-    println!("particles: {:?}", particles);
-    let x_min: i64 = 200000000000000;
-    let x_max: i64 = 400000000000000;
-    let y_min: i64 = 200000000000000;
-    let y_max: i64 = 400000000000000;
 
-    let mut num_intersections = 0;
-    for i in 0..particles.len() {
-        for j in i + 1..particles.len() {
-            let intersection = get_intersection(particles[i], particles[j]);
-            if let Some((x, y)) = intersection {
-                // check if intersection is within bounds
-                if x >= x_min as f64 && x <= x_max as f64 && y >= y_min as f64 && y <= y_max as f64
-                {
-                    num_intersections += 1;
-                }
-            }
-        }
-    }
-    println!("num_intersections: {}", num_intersections);
-    num_intersections
+    let x_min = 200000000000000f64;
+    let x_max = 400000000000000f64;
+    let y_min = 200000000000000f64;
+    let y_max = 400000000000000f64;
+
+    particles
+        .par_iter()
+        .enumerate()
+        .map(|(i, &particle_i)| {
+            particles[i + 1..]
+                .iter()
+                .filter(|&&particle_j| {
+                    is_intersection(particle_i, particle_j, x_min, x_max, y_min, y_max)
+                })
+                .count()
+        })
+        .sum::<usize>() as u32
 }
 
-fn get_intersection(
-    a: ((f64, f64), (f64, f64)),
-    b: ((f64, f64), (f64, f64)),
-) -> Option<(f64, f64)> {
-    let ((x_a, y_a), (x_vel_a, y_vel_a)) = a;
-    let ((x_b, y_b), (x_vel_b, y_vel_b)) = b;
-
-    let a_vel = y_vel_a / x_vel_a;
-    let b_vel = y_vel_b / x_vel_b;
+fn is_intersection(
+    a: ((f64, f64), f64, f64),
+    b: ((f64, f64), f64, f64),
+    x_min: f64,
+    x_max: f64,
+    y_min: f64,
+    y_max: f64,
+) -> bool {
+    let ((x_a, y_a), y_vel_a, a_vel) = a;
+    let ((x_b, y_b), y_vel_b, b_vel) = b;
 
     // check if parallel
     if a_vel == b_vel {
-        return None;
+        return false;
     }
-    let x = (y_b - y_a + a_vel * x_a - b_vel * x_b) / (a_vel - b_vel);
+
+    let mut x = y_b - y_a + a_vel * x_a - b_vel * x_b;
+    if !(x.abs() >= x_min * (a_vel - b_vel).abs() && x.abs() <= x_max * (a_vel - b_vel).abs()) {
+        return false;
+    }
+    x /= a_vel - b_vel;
     let y = a_vel * (x - x_a) + y_a;
 
-    if (x < x_a && x_vel_a > 0.0)
-        || (x > x_a && x_vel_a < 0.0)
+    if !(x >= x_min && x <= x_max && y >= y_min && y <= y_max)
         || (y < y_a && y_vel_a > 0.0)
         || (y > y_a && y_vel_a < 0.0)
-        || (x < x_b && x_vel_b > 0.0)
-        || (x > x_b && x_vel_b < 0.0)
         || (y < y_b && y_vel_b > 0.0)
         || (y > y_b && y_vel_b < 0.0)
     {
-        return None;
+        return false;
     }
 
-    Some((x, y))
+    true
 }
 
-fn parse_input(input: &str) -> Vec<((f64, f64), (f64, f64))> {
+fn parse_input(input: &str) -> Vec<((f64, f64), f64, f64)> {
     input
         .lines()
         .map(|line| {
-            let parts: Vec<&str> = line.split('@').collect();
-            let pos: Vec<f64> = parts[0]
-                .trim()
-                .split(',')
-                .map(|p| p.trim().parse::<i64>().unwrap() as f64)
+            let mut parts = line.split(" @ ");
+            let pos: Vec<f64> = parts
+                .next()
+                .unwrap()
+                .split(", ")
+                .map(|p| p.parse::<i64>().unwrap() as f64)
                 .collect();
-            let vel: Vec<f64> = parts[1]
-                .trim()
-                .split(',')
-                .map(|v| v.trim().parse::<i64>().unwrap() as f64)
+            let vel: Vec<f64> = parts
+                .next()
+                .unwrap()
+                .split(", ")
+                .map(|v| v.parse::<i64>().unwrap() as f64)
                 .collect();
-            ((pos[0], pos[1]), (vel[0], vel[1]))
+            let a_vel = vel[1] / vel[0];
+
+            ((pos[0], pos[1]), vel[1], a_vel)
         })
         .collect()
 }
