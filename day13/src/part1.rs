3d3a5af35abda05eeb4159113c7d19d5f1a00509
igreat <goraguy5@gmail.com>
2023-12-28 17:09:24 +0000

    small code cleanup

--- day13/src/part1.rs
+++ day13/src/part1.rs
@@ -1,14 +1,13 @@
 pub fn run(input: &str) -> usize {
     let islands = parse_input(input);
     let mut result = 0;
+
     let mut prev_ref_idx = Vec::new();
     let mut curr_ref_idx = Vec::new();
-    let mut s1: Vec<u8>;
-    let mut s2: Vec<u8>;
     for island in islands {
+        // HORIZONTAL CHECK
         prev_ref_idx.clear();
         curr_ref_idx.clear();
-        // horizontal
         for line in &island {
             if prev_ref_idx.is_empty() {
                 prev_ref_idx = (1..line.len()).collect();
@@ -29,10 +28,10 @@ pub fn run(input: &str) -> usize {
 
         if prev_ref_idx.len() == 1 {
             result += prev_ref_idx[0];
-            continue;
+            continue; // assuming strictly either horizontal or vertical
         }
 
-        // vertical
+        // VERTICAL CHECK
         prev_ref_idx.clear();
         curr_ref_idx.clear();
         for j in 0..island[0].len() {
@@ -43,8 +42,8 @@ pub fn run(input: &str) -> usize {
                 let ref_len = i.min(island.len() - i);
 
                 // pluck out the two columns
-                s1 = island[(i - ref_len)..i].iter().map(|row| row[j]).collect();
-                s2 = island[i..(i + ref_len)].iter().map(|row| row[j]).collect();
+                let s1: Vec<u8> = island[(i - ref_len)..i].iter().map(|row| row[j]).collect();
+                let s2: Vec<u8> = island[i..(i + ref_len)].iter().map(|row| row[j]).collect();
                 if are_reverse(&s1, &s2) {
                     curr_ref_idx.push(i);
                 }
@@ -67,14 +66,9 @@ fn are_reverse(s1: &[u8], s2: &[u8]) -> bool {
     s1.iter().zip(s2.iter().rev()).all(|(c1, c2)| c1 == c2)
 }
 
-fn parse_input(input: &str) -> Vec<Vec<Vec<u8>>> {
+fn parse_input(input: &str) -> Vec<Vec<&[u8]>> {
     input
         .split("\n\n")
-        .map(|island| {
-            island
-                .lines()
-                .map(|line| line.as_bytes().to_vec())
-                .collect()
-        })
+        .map(|island| island.lines().map(|line| line.as_bytes()).collect())
         .collect()
 }
