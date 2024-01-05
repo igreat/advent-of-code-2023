4b6d6d5bdd3afaa2046828254eea5b58965cca30
igreat <goraguy5@gmail.com>
2024-01-05 17:31:43 +0000

    improve memory efficiency of p1

--- day21/src/part1.rs
+++ day21/src/part1.rs
@@ -3,26 +3,28 @@ use std::collections::VecDeque;
 const NUM_STEPS: usize = 65;
 pub fn run(input: &str) -> usize {
     let grid = parse_input(input);
-    let mut visited = vec![vec![vec![false; grid[0].len()]; grid.len()]; NUM_STEPS];
     let (x, y) = find_start(&grid);
-    bfs(&grid, &mut visited, x, y, NUM_STEPS)
+    bfs(&grid, x, y, NUM_STEPS)
 }
 
-fn bfs(
-    grid: &Vec<Vec<u8>>,
-    visited: &mut Vec<Vec<Vec<bool>>>,
-    x: usize,
-    y: usize,
-    steps: usize,
-) -> usize {
+fn bfs(grid: &Vec<Vec<u8>>, x: usize, y: usize, steps: usize) -> usize {
+    let mut visited = vec![vec![false; grid[0].len()]; grid.len()];
     let mut queue = VecDeque::new();
+    let mut current_distance = 0;
     queue.push_back((x, y, 0));
     while !queue.is_empty() {
         let (x, y, distance) = queue.pop_front().unwrap();
-        if distance >= steps || visited[distance][y][x] {
+        if distance == steps || visited[y][x] {
             continue;
         }
-        visited[distance][y][x] = true;
+        if distance > current_distance {
+            for row in &mut visited {
+                row.fill(false);
+            }
+            current_distance = distance;
+        }
+
+        visited[y][x] = true;
         for (dx, dy) in &[(0, -1), (1, 0), (0, 1), (-1, 0)] {
             let nx = x as i32 + dx;
             let ny = y as i32 + dy;
@@ -37,11 +39,7 @@ fn bfs(
         }
     }
 
-    visited[steps - 1]
-        .iter()
-        .flatten()
-        .filter(|&&cell| cell)
-        .count()
+    visited.iter().flatten().filter(|&&cell| cell).count()
 }
 
 fn parse_input(input: &str) -> Vec<Vec<u8>> {
